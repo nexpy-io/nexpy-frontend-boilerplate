f57c58bcc898b7048dcc64527c702f0586cbc33f
Allan Oliveira Miraballes <allan.miraballes@nave.rs>
2021-10-04 00:26:04 -0300

    feat: add security http haders

--- next.config.js
+++ next.config.js
@@ -5,7 +5,38 @@ const withPWA = require('next-pwa')
 
 const plugins = [withMDX, withPWA]
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+]
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
   reactStrictMode: true,
   poweredByHeader: false,
   pageExtensions: ['tsx', 'mdx'],
