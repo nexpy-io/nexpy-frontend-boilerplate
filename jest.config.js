5b94963a38c0e75099d60e6404a53ae741aea33c
Allan Oliveira <nallamo2016@gmail.com>
2022-01-18 00:42:48 -0300

    test: add fulltest script and tsconfig spec settings

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
+  roots: ['<rootDir>/src'],
+
   // Stop running tests after `n` failures
   // bail: 0,
 
--- src/utils/tests/crypt.test.ts
+++ src/utils/tests/crypt.test.ts
@@ -1,3 +1,5 @@
+/// <reference types="jest" />
+
 import { getObfuscatedToken, getUnobfuscatedToken } from '../crypt'
 
 describe('[ENVS]', () => {
